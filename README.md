3a3cf2f40394fd6bf51f865b9b8d928ac4e2125e
gveliz90 <gveliz90@bancoestado.cl>
2023-03-22 22:25:30 -0300

    modificacion html

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# ComponentesLuciaRocha
+# ComponentesVeliz
 
 This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 15.2.4.
 
--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -16,16 +16,15 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'componentesLuciaRocha'`, () => {
+  it(`should have as title 'componentesVeliz'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('componentesLuciaRocha');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('componentesLuciaRocha app is running!');
+    expect(compiled.querySelector('.content span')?.textContent).toContain('componentesVeliz app is running!');
   });
 });
